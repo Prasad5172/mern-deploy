fb5396cb0596b48d4f97b8d4fe4fe17d531ba52e
prasad <prasadpadala2005@gmail.com>
2023-09-03 16:50:49 +0530

    test

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv")
 dotenv.config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.REACT_APP_DATA_BASE_URL_lOCATHOST);
+mongoose.connect(process.env.REACT_APP_DATA_BASE_URL_MONGODB);
 mongoose.connection.on("connected",() => console.log('Connection Is Succesful'))
 mongoose.connection.on("error",(error) => console.log('Connection Failed With-'+error))
 
